b31194e5db26aa59c0d9956ada9a86882fc2eec0
Rafael Graciano <rafagraciribeiro@gmail.com>
2024-12-19 01:08:37 -0300

    first commit

---
+++ src/frase.ts
@@ -0,0 +1,8 @@
+//Função que retorna string
+function dizOla(nome:string):string { 
+    return `Olá ${nome}`;
+};
+
+let seuNome  = "Genivaldo Pereira"
+const saudar = dizOla(seuNome)
+console.log(saudar)
