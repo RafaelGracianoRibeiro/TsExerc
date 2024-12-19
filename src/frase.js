f5371ca2eacfb72db6d13a9583c53c9a07bf57d4
Rafael Graciano <rafagraciribeiro@gmail.com>
2024-12-19 01:11:47 -0300

    commit 2

--- src/frase.js
+++ src/frase.js
@@ -1,1 +1,9 @@
 "use strict";
+//Função que retorna string
+function dizOla(nome) {
+    return `Olá ${nome}`;
+}
+;
+let seuNome = "Genivaldo Pereira";
+const saudar = dizOla(seuNome);
+console.log(saudar);
---
+++ src/multiplicacao.ts
@@ -0,0 +1,9 @@
+//Criando função multiplicadora
+function Multiplica (valorA: number, valorB: number): number {
+    const multiplicacao  = valorA * valorB
+    return multiplicacao
+}
+
+const resultado = Multiplica(250, 4)
+console.log(resultado)
+
