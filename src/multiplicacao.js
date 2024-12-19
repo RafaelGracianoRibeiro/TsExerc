8e37f59d139a6b38fdd8ea8009c1739a33b2b5bc
Rafael Graciano <rafagraciribeiro@gmail.com>
2024-12-19 01:12:20 -0300

    commit 3

--- src/multiplicacao.js
+++ src/multiplicacao.js
@@ -1,1 +1,8 @@
 "use strict";
+//Criando função multiplicadora
+function Multiplica(valorA, valorB) {
+    const multiplicacao = valorA * valorB;
+    return multiplicacao;
+}
+const resultado = Multiplica(250, 4);
+console.log(resultado);
